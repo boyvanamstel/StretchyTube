901404a32ac79644dd20c1f4b7f5179a55dcbbca
Boy van Amstel <boy@boyvanamstel.nl>
2022-11-29 21:26:11 +0100

    Initial Commit

---
+++ StretchyTube/StretchyTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyTubeApp.swift
+//  StretchyTube
+//
+//  Created by Boy van Amstel on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StretchyTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
